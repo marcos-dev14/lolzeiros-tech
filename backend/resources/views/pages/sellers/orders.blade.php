414760439841df1859bfd8c77c5c74557a33b2bc
marcos-dev14 <mprogramador.10@gmail.com>
2025-01-08 19:22:26 -0300

    feat: inicio do layout da tela do Meus Pedidos

--- backend/resources/views/pages/sellers/orders.blade.php
+++ backend/resources/views/pages/sellers/orders.blade.php
@@ -1,19 +1,24 @@
+@push('styles')
+    <link rel="stylesheet" href="{{ mix('css/orders-seller.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/plugins.css') }}">
+@endpush
+
 <x-layouts.seller-panel
-    title="Clientes"
+    title="Meus Pedidos"
     subtitle="Olá <strong>{{ $seller->name }}</strong> aqui estão todas suas vendas junta a AugeApp."
-    icon="icons.users"
+    icon="icons.bag-seller"
 >
     <div class="container">
         <table id="orders-table" class="table">
             <thead>
                 <tr>
-                    <th>#</th>
                     <th>Codigo</th>
                     <th>Cliente</th>
                     <th>Fornecedor</th>
                     <th>Data</th>
                     <th>Valor</th>
                     <th>Status</th>
+                    <th>Ação</th>
                 </tr>
             </thead>
             <tbody>
@@ -23,9 +28,62 @@
                             <td>{{ $order->code }}</td>
                             <td>{{ $order->cliente }}</td>
                             <td>{{ $order->fornecedor }}</td>
-                            <td>{{ $order->data }}</td>
-                            <td>{{ $order->valor }}</td>
-                            <td>{{ $order->status }}</td>
+                            <td>
+                                <div class="formatted-input">
+                                    <div class="formatted-input-icon">
+                                        <x-icons.calendar></x-icons.calendar>
+                                    </div>
+
+                                    <input type="text" id="date" disabled value="{{ $order->data }}">
+                                </div>
+                              </td>
+                              <td>
+                                <div class="formatted-input">
+                                    <div class="formatted-input-icon">
+                                        <x-icons.money></x-icons.money>
+                                    </div>
+
+                                    <input type="text" id="value" disabled value="{{ $order->valor }}">
+                                </div>
+                            </td>
+
+                            <td>
+                                <div class="status {{
+                                    $order->status === 'Novo' ? 'status-novo' :
+                                    ($order->status === 'Liberado' ? 'status-liberado' :
+                                    ($order->status === 'Recebido' ? 'status-recebido' :
+                                    ($order->status === 'Transmitido' ? 'status-transmitido' :
+                                    ($order->status === 'Cancelado' ? 'status-cancelado' : ''))))
+                                }}">
+                                    @if($order->status === 'Novo' || $order->status === 'Transmitido')
+                                        <x-icons.order-star></x-icons.order-star>
+                                    @elseif($order->status === 'Cancelado')
+                                        <x-icons.order-cancel></x-icons.order-cancel>
+                                    @elseif($order->status === 'Liberado')
+                                        <x-icons.order-free></x-icons.order-free>
+                                    @elseif($order->status === 'Recebido')
+                                        <x-icons.order-free-2></x-icons.order-free-2>
+                                    @endif
+
+                                    {{ $order->status }}
+                                </div>
+                            </td>
+
+                            <td>
+                                <div class="action-buttons">
+                                    <button class="eye">
+                                        <x-icons.eye></x-icons.eye>
+                                    </button>
+
+                                    <button class="printer">
+                                        <x-icons.printer></x-icons.printer>
+                                    </button>
+
+                                    <button class="heart">
+                                        <x-icons.order-heart></x-icons.order-heart>
+                                    </button>
+                                </div>
+                            </td>
                         </tr>
                     @endforeach
                 @endif
@@ -33,3 +91,33 @@
         </table>
     </div>
 </x-layouts.seller-panel>
+
+@push('scripts')
+<script>
+    function formatDate(dateString) {
+      const date = new Date(dateString.split('T')[0]);
+      const day = String(date.getUTCDate()).padStart(2, '0');
+      const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+      const year = date.getUTCFullYear();
+      return `${day}/${month}/${year}`;
+    }
+
+    function formatCurrency(value) {
+      return new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+      }).format(value).replace('R$', '').trim();
+    }
+
+    document.addEventListener('DOMContentLoaded', () => {
+      const dateInput = document.getElementById('date');
+      const valueInput = document.getElementById('value');
+
+      const formattedDate = formatDate('2024-12-22T22:43:00');
+      const formattedValue = formatCurrency(99999.99);
+
+      dateInput.value = formattedDate;
+      valueInput.value = formattedValue;
+    });
+  </script>
+@endpush
