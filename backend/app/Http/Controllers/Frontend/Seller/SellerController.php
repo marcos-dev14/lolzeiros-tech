c19cdf274254d8a5e956278bcd31ab6157d587ce
joseph <joseph.jhowboxer@gmail.com>
2025-01-07 23:52:54 -0300

    novo cliente seller

--- backend/app/Http/Controllers/Frontend/Seller/SellerController.php
+++ backend/app/Http/Controllers/Frontend/Seller/SellerController.php
@@ -5,9 +5,12 @@ namespace App\Http\Controllers\Frontend\Seller;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ClientListCartResource;
 use App\Http\Resources\OrderResource;
+use App\Models\ClientHasSeller;
 use App\Models\CountryState;
 use App\Models\Order;
 use App\Models\Seller;
+use App\Models\Supplier;
+use App\Models\SupplierDiscount;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
@@ -26,55 +29,90 @@ class SellerController extends Controller
             return redirect()->route('login')->with('error', 'Você precisa estar autenticado para acessar esta página.');
         }
 
-        $clientData = $seller->ClientHasSeller->flatMap(function ($clientHasSeller) {
-            $groupName = $clientHasSeller->clientGroup->name ?? null;
-            $supplierName = $clientHasSeller->supplier->company_name
-                ?? $clientHasSeller->supplier->name
-                ?? null;
-            $lastLogin = Carbon::parse($clientHasSeller->clientGroup->buyer->last_login);
+        $clients = $seller->ClientHasSeller->map(function ($clientHasSeller) use ($seller) {
+            $lastClient = null;
+            $clientLastOrder = null;
 
-            return $clientHasSeller->clientGroup->clients->map(function ($client) use ($groupName, $supplierName, $lastLogin) {
-                $mainAddress = $client->getMainAddress();
-                $state = $mainAddress?->country_state_id
-                    ? CountryState::find($mainAddress->country_state_id)
-                    : null;
+            // Verifica o cliente com o último pedido mais recente entre todos os clientes do grupo
+            if ($clientHasSeller->clientGroup && $clientHasSeller->clientGroup->clients) {
+                $clientHasSeller->clientGroup->clients->each(function ($client) use (&$clientLastOrder, &$lastClient) {
+                    $lastOrder = $client->orders->last();
+                    if ($lastOrder && (!$clientLastOrder || $lastOrder->created_at > $clientLastOrder->created_at)) {
+                        $clientLastOrder = $lastOrder;
+                        $lastClient = $client;
+                    }
+                });
+            }
 
-                return (object) [
-                    'grupo' => $groupName,
-                    'fornecedor' => $supplierName,
-                    'cliente' => $client->company_name ?? $client->name,
-                    'cnpj' => $client->document ?? null,
-                    'estado' => $state ? "{$state->name} - {$state->code}" : null,
-                    'cadastro' => Carbon::parse($client->auge_register)->format('d/m/Y H:i'),
-                    'ultimologin' => $lastLogin->format('d/m/Y H:i') . 'h (' . $lastLogin->diffForHumans() . ')',
-                    'carrinhoabandonado' => $client->cart?->products?->count()
-                        ? (new ClientListCartResource($client->cart))->created_at
-                        : null,
-                    'status' => $client->document_status,
+            if (!$lastClient) {
+                $lastClient = $clientHasSeller->clientGroup->clients->first();
+            }
+
+            $mainAddress = $lastClient?->getMainAddress();
+            $state = $mainAddress?->country_state_id
+                ? CountryState::find($mainAddress->country_state_id)
+                : null;
+
+            $lastLogin = Carbon::parse($clientHasSeller->clientGroup->buyer->last_login ?? now());
+            $suppliers = Supplier::query()
+                ->where('is_available', 1)
+                ->where('suspend_sales', 0)
+                ->where('service_migrate', 'Ativado')
+                ->has('installmentRules')
+                ->get();
+
+            // Mapeamento e correção no array de fornecedores
+            $supplierData = $suppliers->map(function ($supplier) use ($clientHasSeller, $seller, $lastClient) {
+                $isSellerClient = ClientHasSeller::where('client_group_id', $clientHasSeller->clientGroup->id)
+                    ->where('supplier_id', $supplier['id'])
+                    ->first();
+                $mainAddress = $lastClient?->getMainAddress();
+                $clientStateCode = $mainAddress?->state?->code;
+               
+                $stateDiscountFromClientState = $supplier->stateDiscounts()->whereHas('states', function ($query) use ($clientStateCode) {
+                    $query->where('code', $clientStateCode);
+                })->first();
+
+                $icmsDiscount = 0;
+                if ($stateDiscountFromClientState instanceof SupplierDiscount) {
+                    $icmsDiscount += floatval($stateDiscountFromClientState->discount_value);
+                    $icmsDiscount += floatval($stateDiscountFromClientState->additional_value);
+                }
+
+                $clientProfile = $lastClient?->client_profile_id;
+                $supplierDiscounts = $supplier->profileDiscounts;
+                $discountToClientProfile = $supplierDiscounts->where('client_profile_id', $clientProfile)->first();
+
+                return [
+                    'name' => $supplier['name'],
+                    ...($isSellerClient && ($isSellerClient->seller_id == $seller->id || $isSellerClient->seller_id === null) ?
+                        ['icms' => $icmsDiscount, 'profileDiscount' => $discountToClientProfile->discount_value ?? 0] : []),
                 ];
             });
+
+            return [
+                'name' => $clientHasSeller->clientGroup->company_name ?? $clientHasSeller->clientGroup->name,
+                'cnpj' => $lastClient?->document,
+                'suppliers' => $supplierData,
+                'state' => $state ? "{$state->name} - {$state->code}" : null,
+                'register' => Carbon::parse($lastClient?->auge_register)->format('d/m/Y H:i'),
+                'lastLogin' => $lastLogin->format('d/m/Y H:i') . 'h (' . $lastLogin->diffForHumans() . ')',
+                'cartAbandoned' => $lastClient?->cart ? (new ClientListCartResource($lastClient?->cart))->created_at : null,
+                'status' => $lastClient?->document_status,
+                'clients' => $clientHasSeller->clientGroup->clients,
+            ];
         });
 
-        $clientData = $clientData->groupBy(function ($client) {
-            return $client->cnpj . '-' . $client->grupo;
-        })->map(function ($group) {
-            $mergedSupplier = $group->pluck('fornecedor')->unique()->implode(', ');
-            $firstClient = $group->first();
-            $firstClient->fornecedor = $mergedSupplier;
-
-            return $firstClient;
-        });
-
-        $clientDataPaginated = $clientData->forPage(request()->get('page', 1), 15);
-        $total = $clientData->count();
 
         return view('pages.sellers.clients', [
             'seller' => $seller,
-            'clientData' => $clientDataPaginated,
-            'total' => $total,
-            'perPage' => 15,
+            'clients' => $clients,
         ]);
     }
+
+
+
+
 
     public function orders()
     {
@@ -190,7 +228,7 @@ class SellerController extends Controller
 
         $clientDataPaginated = $clientData->forPage(request()->get('page', 1), 15);
         $total = $clientData->count();
-        //dd($clientDataPaginated);
+        dd($clientDataPaginated);
         return view('pages.sellers.abandonedCarts', [
             'seller' => $seller,
             'clientData' => $clientDataPaginated,
